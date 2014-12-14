4c0187b1754a827fa959194db4ed7da1f90d5cfc
lokendrashukla <lok7.shukla7@gmail.com>
2014-12-14 12:41:52 +0530

    Updated example in index file

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  * 
  * Create issues on github and bitbucket
  * Examples :- php index.php create-issue -u lokendrashukla https://github.com/lokendrashukla/issueCreator "Issue 1" "This is a sample issue."
+ * Examples :- php index.php create-issue -u lokendrashukla https://bitbucket.org/lokendrashukla/issueCreator "Issue 1" "This is a sample issue."
  * @author Lokendra Shukla
  */
 define('APPLICATION_PATH', dirname(__FILE__));
